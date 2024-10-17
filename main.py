79700526f6b7f01485f770249a2a31c112c9ba02
Adrian Edwards <adrian@adriancedwards.com>
2024-10-17 09:23:46 -0400

    fix it

--- main.py
+++ main.py
@@ -7,18 +7,13 @@ def extract_highlighted_text(pdf_file):
 
     highlights = []
 
-    # Iterate over all pages
-    for page_num in range(doc.page_count):
-        page = doc.load_page(page_num)
-        # Extract annotations (highlights)
-        annotations = page.annots()
+    for page in doc:
+        for annot in page.annots():
+            # https://github.com/pymupdf/PyMuPDF/discussions/1573
+            text = page.get_textbox(annot.rect)
+            if annot.type[0] == 8:  # 8 corresponds to Highlight annotations
+                highlights.append(text)
 
-        if annotations:
-            for annot in annotations:
-                if annot.info["subtype"] == "Highlight":
-                    # Get the highlighted text
-                    highlight = page.get_text("highlight", annot)
-                    highlights.append(highlight)
 
     return highlights
 
