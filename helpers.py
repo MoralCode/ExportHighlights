efd4e8bfc113c9c5df3a7911133d40e3acc9149b
Adrian Edwards <adrian@adriancedwards.com>
2024-10-17 13:10:31 -0400

    optionally resort by a different key

--- helpers.py
+++ helpers.py
@@ -1,8 +1,14 @@
 import itertools
 
-def split_dict_list(dictlist, splitter):
+def split_dict_list(dictlist, splitter, sortkey=None):
     """ Split a dictionary into a list of dictionaries based on the results of a given function. """
     # sort by key
     dictlist.sort(key=splitter)
     # split by key
-    return [list(g) for k, g in itertools.groupby(dictlist, key=splitter)]
+    split = [list(g) for k, g in itertools.groupby(dictlist, key=splitter)]
+
+    if sortkey:
+        # sort each sublist by sortkey
+        split = [sorted(sublist, key=sortkey) for sublist in split]
+
+    return split
